be46ee43546aa81e994787537471ada1e1c589fd
seungdae.goh <wingjoy77@gmail.com>
2019-09-09 01:00:56 +0900

    add practice of argv

---
+++ p04_argv.py
@@ -0,0 +1,17 @@
+import sys
+
+
+param_n = len(sys.argv);
+
+if  (param_n >= 2) :
+	print(sys.argv[1]) 
+
+if  (param_n >= 3) :
+	print(sys.argv[2]) 
+
+if  (param_n >= 4) :
+	print(sys.argv[3]) 
+
+
+print("End of Program");
+
