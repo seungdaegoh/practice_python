998366f79a3ba8c59fbd7fed588f23157263d2d7
seungdae.goh <seungdae.goh@lge.com>
2019-09-18 15:09:35 +0900

    argv test

--- p04_argv.py
+++ p04_argv.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 
 '''
 
@@ -14,5 +16,55 @@ if  (param_n >= 4) :
 	print(sys.argv[3]) 
 '''
 
-print("End of Program");
+
+
+
+import argparse
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument("-v", "--verbosity", help="increase output verbosity",
+                            action="count")
+
+parser.add_argument('--op', type=str, default='add',
+                    choices=['add','sub','mul','div'],
+                                help='What operation?')
+
+parser.add_argument("left", type=int, help="input l-value which will be used by operation")
+parser.add_argument("right", type=int, help="input r-value which will be used by operation")
+
+
+
+args = parser.parse_args()
+
+
+'''
+if args.verbosity == 1:
+    print("{}^2 == {}".format(args.square, answer))
+elif args.verbosity == 2:
+    print("the square of {} equals {}".format(args.square, answer))
+else:
+    print("Ans: ", answer)
+
+#print(args.square**2)
+'''
+
+lv = args.left
+rv = args.right
+op = args.op
+
+if op == 'add' :
+    ans = lv + rv
+
+elif op == 'sub' :
+    ans = lv - rv
+
+elif op == 'mul' :
+    ans = lv * rv
+
+elif op == 'div' :
+    ans = lv / rv
+
+print("Answer = ", ans)
+print("End of Program")
 
