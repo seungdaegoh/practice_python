921bb21b8f2dc00615e567d8497b8a28435a062a
seungdae.goh <wingjoy77@gmail.com>
2019-09-29 23:59:13 +0900

    done with find

--- p06_find.py
+++ p06_find.py
@@ -4,17 +4,45 @@
 import argparse
 
 parser = argparse.ArgumentParser(prog='find', description='find files' )
-parser.add_argument('dir',  metavar='N', type=str, nargs='+')
-parser.add_argument('-name',  metavar='N', type=str, nargs='+')
-parser.add_argument('-print',  metavar='N', type=str, nargs='+')
+parser.add_argument('dir',  type=str, nargs='+')
+parser.add_argument('-name',  metavar='dir name', type=str)
+parser.add_argument('-print',  action="store_true")
 
 args = parser.parse_args()
 
-	print( "args.file = ", args.file )
+# print( "args.dir = ", args.dir[0] )
+# print( "args.name = ", args.name)
 
+
+# print(type(args.dir[0]))
+# print(type(args.name))
+
+full_name = args.dir[0] + args.name
+
+# print(full_name)
 
 
 import glob
-print(glob.glob("/home/adam/*.txt")
+flist = glob.glob(full_name)
 
-print('End of program')
+#print(flist)
+
+
+for fname in flist:
+
+	print(fname)
+
+	if args.print :
+		print(" print file's contents -------------")
+
+		f = open(fname, 'r')
+		lines = f.readlines()
+
+		for line in lines:
+			print(line, end = '')
+
+		f.close()
+
+
+
+# print('End of program')
