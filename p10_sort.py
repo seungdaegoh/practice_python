0a826ff3684951ede7dc9437f0e29a284a940f78
seungdae.goh <seungdae.goh@lge.com>
2019-10-07 11:06:33 +0900

    sort more job

--- p10_sort.py
+++ p10_sort.py
@@ -1,5 +1,5 @@
-#! /home/peter/anaconda3/bin/python3
 #! /usr/bin/python3
+#! /home/peter/anaconda3/bin/python3
 
 import argparse
 import sys
@@ -9,30 +9,39 @@ parser = argparse.ArgumentParser(prog='sort.py',
 parser.add_argument('-r', action='store_true', default=False,
 	                    dest='reverse', help="reverse sort")
 
+parser.add_argument('-f', '--ignore-case', action='store_true', default=False,
+	                    dest='ignore_case', help="fold lower case to upper case characters")
+
+parser.add_argument('-g', '--general-numeric-sort',  action='store_true', default=False,
+	                    dest='num_sort', help="compare according to general numerical value")
+
 args = parser.parse_args()
+
+
+print("args.num_sort", args.num_sort, "args.ignore_case", args.ignore_case)
 
 in_method = sys.stdin
 in_lines = [];
 
 for line in in_method:
-	if ( "\n"  not in line ) :
-		print ("____end of input___")
-		break
+    if ( "\n"  not in line ) :
+        print ("____end of input___")
+        break
 
-	in_lines.append(line)
+    in_lines.append(line)
 
-#in_lines.sort();
-
-if (args.reverse) :
-	lines = sorted( in_lines, reverse=True)
-else :
-	lines = sorted( in_lines )
+    if (args.num_sort):
+        lines = sorted( in_lines, reverse=args.reverse, key=int)
+    elif (args.ignore_case) :
+        lines = sorted( in_lines, reverse=args.reverse, key=str.lower)
+    else :
+        lines = sorted( in_lines, reverse=args.reverse)
 
 #print(in_lines);
 
 for word in lines:
 #print("  idx:", i, end=' ')
-	print(word, end='')
+    print(word, end='')
 #i += 1
 
 #print('End of program')
