9e002d3c5cbf75b9b2f6657c4dcb36fd08906d7a
seungdae.goh <wingjoy77@gmail.com>
2019-12-07 21:28:37 +0900

    suffix_array

--- p22_surfix_array/suffix_array.py
+++ p22_surfix_array/suffix_array.py
@@ -14,14 +14,23 @@ class  suffix_array(object):
 
     def find_shortest(self, word):
 
+        w_len = len(word)
+        for i in range(len(self.array) -1 , -1, -1):
+            if (word == self.array[i][:w_len]):
+                return self.array[i]
+
         return None
 
 
 
     def find_longest(self, word):
 
+        w_len = len(word)
+        for i in range(len(self.array)):
+            if (word == self.array[i][:w_len]):
+                return self.array[i]
 
-        return
+        return None
 
 
 
@@ -34,8 +43,12 @@ class  suffix_array(object):
                 #print("__SEARCHED_WORD:", self.array[i])
                 fw.append( self.array[i] )
 
-        return fw
-   
+        if (len(fw) > 0):
+            return fw
+        else:
+            return None
+  
+
 
     def list(self, msg = None):
 
--- p22_surfix_array/test_find_api.py
+++ p22_surfix_array/test_find_api.py
@@ -12,10 +12,26 @@ def test_list():
 
 def test_find_shortest():
 
+    sa = suffix_array("abcddeeeeabcddeefghabcd")
+
+    word = sa.find_shortest("abc")
+    assert(word == "abcd")
+
+    word = sa.find_shortest("azi")
+    assert(word == None)
+
     return
 
-def test_find_logest():
 
+def test_find_longest():
+
+    sa = suffix_array("abcdeabcddeefghabcd")
+
+    word = sa.find_longest("abc")
+    assert(word == "abcdeabcddeefghabcd")
+
+    word = sa.find_longest("azi")
+    assert(word == None)
     return
 
 def test_find_all():
@@ -27,6 +43,11 @@ def test_find_all():
     assert(all_word[0] == "abcdefabc")
     assert(all_word[1] == "abc")
     print("ALL_WORD=", all_word)
+
+
+    all_word = sa.find_all("azi")
+    assert(all_word == None)
+
     print("done with find_all")
     return
 
