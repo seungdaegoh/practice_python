aacfa2813603301093a92b71671f78f662c41706
seungdae.goh <seungdae.goh@lge.com>
2020-01-09 10:51:36 +0900

    psncs

--- psncs/200109_scale/sol.py
+++ psncs/200109_scale/sol.py
@@ -2,42 +2,19 @@
 
 K =  int(input())
 weights = list( map(int, input().split()))
-#print("K=", K)
-#print(weights)
 
-sum_v = 0
+sum_v =  sum(weights)
 run_wt = [0]
 
 for i in weights:
-    sum_v += i
     run_wt1 = [x + i for x in run_wt]
     run_wt2 = [x - i for x in run_wt]
 
     run_wt = list( set(run_wt + run_wt1 + run_wt2) )
 
-    #print("new run_wt = ", run_wt)
-
 run_wt.sort()
 
+size_all = len( run_wt) // 2
 
-for i, w in enumerate(run_wt):
-    if (w == 0):
-        run_wt = run_wt[i + 1:]
-        break
-
-#print("run_wt = ", run_wt)
-
-idx = 0
-cnt = 0
-for i in range(1, sum_v):
-
-
-   if (i != run_wt[idx]):
-       #print("Empty weight = ", i, "cmp_v=", run_wt[idx])
-       cnt += 1
-   else:
-       idx += 1
-
-
+cnt = sum_v - size_all
 print(cnt)
-
